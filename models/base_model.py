9c5d709bb3e657364b3aa2a85d2dfdee5f8023cb
gavintobin <gavintobin714@gmail.com>
2023-02-03 12:00:57 -0800

    added comments to task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,22 +5,25 @@ from datetime import datetime
 
 
 class BaseModel:
+    """basemodel class"""
     def __init__(self, id=0, created_at=0, updated_at=0):
         self.id = str(uuid.uuid4)
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
     def __str__(self):
+        """sets to tring format"""
         strect = "[{}]".format(self.__class__)
         strect += "({})".format(self.id)
         strect += "{}".format(self.__dict__)
         return strect
 
     def save(self):
+        """saves time object was created"""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-
+        """dictionary func"""
         mydict = self.__dict__
         mydict["__class__"] = self.__class__
         mydict["created_at"] = self.created_at.isoformat()
