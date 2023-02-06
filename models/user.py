99485a4d5b083f48e1a09a790283e09f59655995
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-06 14:50:04 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-#!
-=======
 #!/usr/bin/python3
 """user model class"""
 from models.base_model import BaseModel
@@ -12,4 +9,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
->>>>>>> e318bf7f2e95ef9220c9d0938b822b5ddbc0d16a
